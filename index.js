d142ab26e03408bd72b7d30a172cc5ac0dba2f8b
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-10-21 16:36:01 -0400

    improve basic backend

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const path = require('path');
+const app = require('express')();
 
-const app = require('express')()
-  .get('*', (req, res) => {
+app.get('*', (req, res) => {
     if (req.path.includes('teacher/block')) res.sendFile(path.join(__dirname, '/pages/teacher_block.html'))
     else if (req.path.includes('parent/block')) res.sendFile(path.join(__dirname, '/pages/parent_block.html'))
     else if (req.path.includes('parent/pause')) res.sendFile(path.join(__dirname, '/pages/parent_pause.html'))
     else if (req.path.includes('x3/block')) res.sendFile(path.join(__dirname, '/pages/x3_block.html'))
-    else if (req.path.includes('__dev')) res.status(500).sendFile(path.join(__dirname + '/note.txt'))
     else res.sendFile(path.join(__dirname + '/pages/admin_block.html'))
-  })
-  .listen(8080, () => console.log('i love goguardian security xd'));
+}).listen(8080, () => {
+    console.log('GoGuardian Blocked Clone started! <3 goguardian');
+});
